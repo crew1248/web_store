356da059deab341f820a0e561235846a22f5c290
timur <crew1251@yandex.ru>
2016-01-05 22:13:14 +0300

    см

---
+++ x_nova_template/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="x_nova_template.MvcApplication" Language="C#" %>
---
+++ x_nova_template/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(x_nova_template.Startup))]
+namespace x_nova_template
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
