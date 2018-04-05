1ffa16316f3a238066e72f4cbb94b70176a61879
timur <crew1251@yandex.ru>
2018-04-05 14:26:56 +0300

    asd

--- x_nova_template/Areas/Admin/Controllers/AccountController.cs
+++ x_nova_template/Areas/Admin/Controllers/AccountController.cs
@@ -34,7 +34,7 @@ namespace x_nova_template.Areas.Admin.Controllers
 
         public ApplicationSignInManager SignInManager
         {
-            //a
+            //aa
             get
             {
                 return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
